9e87d060d9e69cb8d66064aba49a99c1f10b91e6
fioriDesktop <fiorigaming94@gmail.com>
2019-01-31 12:26:12 +0000

    Mesh Generator

---
+++ Mesh Generator/Assets/MeshGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+    private Mesh mesh;
+
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,1,0),
+            new Vector3(1,1,0),
+            new Vector3(1,0,0),
+
+            new Vector3(0,0,0),
+            new Vector3(0,1,0),
+            new Vector3(0,1,1),
+            new Vector3(0,0,1)
+        };
+
+        triangles = new int[]
+        {
+            0,1,2,
+            0,2,3,
+
+            6,5,4,
+            7,6,4
+        };
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+}
