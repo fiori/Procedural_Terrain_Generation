eb6245e656e9be740ce02f2d4dea851c17e2bd46
fioriDesktop <fiorigaming94@gmail.com>
2019-02-21 17:32:54 +0000

    Working on the report

---
+++ LandMass Generation/Assets/Editor/MeshGeneratorEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(newMeshGenerator))]
+public class MeshGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        newMeshGenerator mesh = (newMeshGenerator) target;
+
+        if (GUILayout.Button("RenderMesh"))
+        {
+            mesh.CreateMesh();
+        }
+    }
+}
---
+++ LandMass Generation/Assets/Scripts/newMeshGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class newMeshGenerator : MonoBehaviour
+{
+    public int MeshWidth;
+    public int MeshHeight;
+
+    private Vector3[] vertices;
+    private int[] triangles;
+    private int triangleIndex;
+
+    void CalculateMesh(int width, int height)
+    {
+        vertices = new Vector3[width * height];
+        triangles = new int[(width-1)*(height-1)*6];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                
+            }
+        }
+    }
+
+    void AddTriangles(int a, int b, int c)
+    {
+        triangles[triangleIndex] = a;
+        triangles[triangleIndex + 1] = b;
+        triangles[triangleIndex + 2] = c;
+        triangleIndex += 3;
+    }
+
+    public Mesh CreateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        return mesh;
+    }
+
+}
